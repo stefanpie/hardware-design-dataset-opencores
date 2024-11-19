42cea13bbe29dbf5f3a05ba583fabb23052f3aef
Stefan Abi-Karam <stefanabikaram@gmail.com>
2024-11-19 03:26:17 -0500

    split usbhostslave design

--- designs/usbhostslave__usb_host/slaveRxStatusMonitor.v
+++ designs/usbhostslave__usb_host/slaveRxStatusMonitor.v
@@ -1,12 +1,12 @@
 //////////////////////////////////////////////////////////////////////
 ////                                                              ////
-//// usbHostCyc2Wrap_usb1t11.v                                            ////
+//// slaveRxStatusMonitor.v                                       ////
 ////                                                              ////
 //// This file is part of the usbhostslave opencores effort.
 //// <http://www.opencores.org/cores//>                           ////
 ////                                                              ////
 //// Module Description:                                          ////
-////   Top level module wrapper. 
+//// 
 ////                                                              ////
 //// To Do:                                                       ////
 //// 
@@ -16,7 +16,7 @@
 ////                                                              ////
 //////////////////////////////////////////////////////////////////////
 ////                                                              ////
-//// Copyright (C) 2008 Steve Fielding and OPENCORES.ORG          ////
+//// Copyright (C) 2004 Steve Fielding and OPENCORES.ORG          ////
 ////                                                              ////
 //// This source file may be used and distributed without         ////
 //// restriction provided that this copyright statement is not    ////
@@ -43,109 +43,53 @@
 //
 `include "timescale.v"
 
+module slaveRxStatusMonitor(connectStateIn, connectStateOut, resumeDetectedIn, resetEventOut, resumeIntOut, clk, rst);
 
-module usbHostCyc2Wrap_usb1t11(
-  clk_i, 
-  rst_i,
-  address_i, 
-  data_i, 
-  data_o, 
-  we_i, 
-  strobe_i,
-  ack_o,
-  irq, 
-  usbClk,
-  USBWireVPin,
-  USBWireVMin,
-  USBWireVPout,
-  USBWireVMout,
-  USBWireOE_n,
-  USBFullSpeed
-   );
+input [1:0] connectStateIn;
+input resumeDetectedIn;
+input clk;
+input rst;
+output resetEventOut;
+output [1:0] connectStateOut;
+output resumeIntOut;
 
-input clk_i;
-input rst_i;
-input [7:0] address_i; 
-input [7:0] data_i; 
-output [7:0] data_o; 
-input we_i; 
-input strobe_i;
-output ack_o;
-output irq; 
-input usbClk;
-input USBWireVPin /* synthesis useioff=1 */;
-input USBWireVMin /* synthesis useioff=1 */;
-output USBWireVPout /* synthesis useioff=1 */;
-output USBWireVMout /* synthesis useioff=1 */;
-output USBWireOE_n /* synthesis useioff=1 */;
-output USBFullSpeed /* synthesis useioff=1 */;
+wire [1:0] connectStateIn;
+wire resumeDetectedIn;
+reg resetEventOut;
+reg [1:0] connectStateOut;
+reg resumeIntOut;
+wire clk;
+wire rst;
 
-wire clk_i;
-wire rst_i;
-wire [7:0] address_i; 
-wire [7:0] data_i; 
-wire [7:0] data_o; 
-wire irq;
-wire usbClk;
-wire USBWireDataOutTick;
-wire USBWireDataInTick;
-wire USBFullSpeed;
+reg [1:0]oldConnectState;
+reg oldResumeDetected;
 
-//internal wiring 
-wire hostSOFSentIntOut; 
-wire hostConnEventIntOut; 
-wire hostResumeIntOut; 
-wire hostTransDoneIntOut;
-wire slaveSOFRxedIntOut; 
-wire slaveResetEventIntOut; 
-wire slaveResumeIntOut; 
-wire slaveTransDoneIntOut;
-wire slaveNAKSentIntOut;
-wire USBWireCtrlOut;
-wire [1:0] USBWireDataIn;
-wire [1:0] USBWireDataOut;
+always @(connectStateIn)
+begin
+  connectStateOut <= connectStateIn;
+end
 
 
-assign irq = hostSOFSentIntOut | hostConnEventIntOut |
-             hostResumeIntOut | hostTransDoneIntOut;
-
-assign USBWireDataIn = {USBWireVPin, USBWireVMin};
-assign {USBWireVPout, USBWireVMout} = USBWireDataOut;
-assign USBWireOE_n = ~USBWireCtrlOut;
-
-//Parameters declaration: 
-defparam usbHostInst.HOST_FIFO_DEPTH = 64;
-parameter HOST_FIFO_DEPTH = 64;
-defparam usbHostInst.HOST_FIFO_ADDR_WIDTH = 6;
-parameter HOST_FIFO_ADDR_WIDTH = 6;
-
-usbHost usbHostInst (
-  .clk_i(clk_i),
-  .rst_i(rst_i),
-  .address_i(address_i),
-  .data_i(data_i),
-  .data_o(data_o),
-  .we_i(we_i),
-  .strobe_i(strobe_i),
-  .ack_o(ack_o),
-  .usbClk(usbClk),
-  .hostSOFSentIntOut(hostSOFSentIntOut),
-  .hostConnEventIntOut(hostConnEventIntOut),
-  .hostResumeIntOut(hostResumeIntOut),
-  .hostTransDoneIntOut(hostTransDoneIntOut),
-  .USBWireDataIn(USBWireDataIn),
-  .USBWireDataInTick(USBWireDataInTick),
-  .USBWireDataOut(USBWireDataOut),
-  .USBWireDataOutTick(USBWireDataOutTick),
-  .USBWireCtrlOut(USBWireCtrlOut),
-  .USBFullSpeed(USBFullSpeed));
-
+always @(posedge clk)
+begin
+  if (rst == 1'b1)
+  begin
+    oldConnectState <= connectStateIn;
+    oldResumeDetected <= resumeDetectedIn;
+  end
+  else
+  begin
+    oldConnectState <= connectStateIn;
+    oldResumeDetected <= resumeDetectedIn;
+    if (oldConnectState != connectStateIn)
+      resetEventOut <= 1'b1;
+    else
+      resetEventOut <= 1'b0;
+    if (resumeDetectedIn == 1'b1 && oldResumeDetected == 1'b0)
+      resumeIntOut <= 1'b1;
+    else 
+      resumeIntOut <= 1'b0;
+  end
+end
 
 endmodule
-
-  
-  
-
-
-
-
